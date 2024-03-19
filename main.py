d72c25a9b066e4fe0f5bb48e906bc99e3cc0b7b2
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 22:19:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+name = "Billy"
+print("We want to know if you like progamming!")
+print(name)
+print("Do you like programming " + name + "?")
+answer =("yes")
+print("Great, " + name + ", you said " + answer + "!")
+print("Let's learn some Python today")
+
+# How many output statements are used in the code?
+ # Answer:There's like 5 outputs in this code 
+
+# What symbol is used for concatenation?
+ # Answer: The symbol use is the plu's (+) sign 
+
+# What is the purpose of the program?
+ # Answer: The purpose of the program is to ask a questions and at the same time use the variable assigned to asnwer 
+
+# What line(s) is assignment used on?
+ # Answer:lines 1 and 5 
+
+# What symbol is used for assignment?
+ # Answer: the symbol equal (=)
+
+# What is the purpose of the empty print statement on line 3?
+ # Answer: the purpose is for us to add the variable name on it to complete the sentence
